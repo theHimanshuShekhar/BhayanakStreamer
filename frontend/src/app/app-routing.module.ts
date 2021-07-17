3c445f1c727c777c059c384868a1a58c15629ade
theHimanshuShekhar <hshekhar.dev@gmail.com>
2021-07-17 15:18:19 +0530

    added modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./directory/directory.module').then(m => m.DirectoryModule) },
   { path: 'room', loadChildren: () => import('./room/room.module').then(m => m.RoomModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: "**", redirectTo: ''}
 ];
 
